e2af6d045e2a272cafc9ac037f0219ca8a8f0d3b
Anantharaman A <105240317+ananth-a-1996@users.noreply.github.com>
2022-05-15 17:37:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice=streamlit.text_input('What fruit would you like information about ?')
-streamlit.write('user entered',apple)
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "apple")
+streamlit.write('user entered',fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 
 
 
