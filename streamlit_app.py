dbda2607cdf72856792e859faa05d6622ad27033
Anantharaman A <105240317+ananth-a-1996@users.noreply.github.com>
2022-05-15 17:38:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,6 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "fruit_ch
 
 
 
-
+fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
